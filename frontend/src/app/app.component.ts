772a16a97d18b943456e09aadb98b51d24657fc4
yassinebouchida <yassine.bouchida@outlook.com>
2025-05-26 11:30:37 +0100

    Fix frontend as normal folder

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: false,
+  template: `
+    <app-home></app-home>`
+})
+export class AppComponent {}
