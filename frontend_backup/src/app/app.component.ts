42b292df888b26a78a2d749743d59038e42a3973
Yassinebouchida <128947592+Yassinebouchida@users.noreply.github.com>
2025-10-22 21:15:54 +0100

    Delete frontend_backup directory

--- frontend_backup/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  standalone: false,
-  template: `
-    <app-home></app-home>`
-})
-export class AppComponent {}
